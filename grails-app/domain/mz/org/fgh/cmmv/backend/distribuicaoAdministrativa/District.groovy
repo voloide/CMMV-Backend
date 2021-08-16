9ca31a823319b2160c27ecd4a2971c7c147949fd
Colaco Nhongo <colaco.nhango@fgh.org.mz>
2021-08-16 15:20:10 +0200

    Add Data Encrypt functionality on Domain Classes

--- grails-app/domain/mz/org/fgh/cmmv/backend/distribuicaoAdministrativa/District.groovy
+++ grails-app/domain/mz/org/fgh/cmmv/backend/distribuicaoAdministrativa/District.groovy
@@ -1,5 +1,7 @@
 package mz.org.fgh.cmmv.backend.distribuicaoAdministrativa
 
+import com.bloomhealthco.jasypt.GormEncryptedStringType
+
 class District {
 
     String code
@@ -10,5 +12,21 @@ class District {
     static constraints = {
         code nullable: false, unique: ['province']
         description nullable: false
+    }
+
+    static mapping = {
+        code type: GormEncryptedStringType
+        description type: GormEncryptedStringType
+//        birthDate type: GormEncryptedDateAsStringType
+//        anniversary type: GormEncryptedCalendarAsStringType
+//        hasInsurance type: GormEncryptedBooleanType
+//        latitude type: GormEncryptedDoubleAsStringType
+//        cashBalance type: GormEncryptedFloatAsStringType
+//        bdBalance type: GormEncryptedBigDecimalType
+//        bdBalance type: GormEncryptedBigDecimalAsStringType
+//        weight type: GormEncryptedShortAsStringType
+//        height type: GormEncryptedIntegerAsStringType
+//        patientId type: GormEncryptedLongAsStringType
+//        biteMe type: GormEncryptedByteAsStringType
     }
 }
